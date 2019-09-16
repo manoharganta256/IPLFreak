4f86fac56859b56f07d1800c805c0d5739d93592
Manohar Ganta <manoharganta256@gmail.com>
2019-09-17 04:16:24 +0530

    Admin model change, Security fix

--- IPL/settings.py
+++ IPL/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y%luml)85ub1dlg6crl#-)0(6tz_!=-7i9l1)!46+sx%*k^1tt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
--- iplfreak/admin.py
+++ iplfreak/admin.py
@@ -3,6 +3,18 @@ from iplfreak.models import Match, Deliveries, UserProfile
 
 
 # Register your models here.
-admin.site.register(Match)
-admin.site.register(Deliveries)
-admin.site.register(UserProfile)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ['match_id', 'season', 'team1', 'team2', 'winner', 'venue']
+
+
+class DeliveriesAdmin(admin.ModelAdmin):
+    list_display = ['match_id', 'innings', 'batsman', 'bowler', 'batsman_runs', 'extra_runs', 'player_dismissed']
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'profile_picture']
+
+
+admin.site.register(Match, MatchAdmin)
+admin.site.register(Deliveries, DeliveriesAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
